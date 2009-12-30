d757cfef7068d338d6d14ceaed608fdf941f5ea5
Josh Close <narshe@gmail.com>
2009-12-30 16:14:22 -0600

    Added license to files missing it.

--- src/CsvHelper/CsvFieldAttribute.cs
+++ src/CsvHelper/CsvFieldAttribute.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿#region License
+// Copyright 2009 Josh Close
+// This file is a part of CsvHelper and is licensed under the MS-PL
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html
+#endregion
+using System;
 using System.Diagnostics;
 
 namespace CsvHelper
--- src/CsvHelper/CsvFieldAttributeComparer.cs
+++ src/CsvHelper/CsvFieldAttributeComparer.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿#region License
+// Copyright 2009 Josh Close
+// This file is a part of CsvHelper and is licensed under the MS-PL
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html
+#endregion
+using System.Collections.Generic;
 
 namespace CsvHelper
 {
