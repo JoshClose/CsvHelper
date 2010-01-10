5762cc88cbb2564ed174fd661e235154ee316989
Josh Close <narshe@gmail.com>
2010-01-10 01:13:49 -0600

    Added files that were missing after combining commits.

---
+++ src/CsvHelper/CsvParserOptions.cs
@@ -0,0 +1,53 @@
+﻿#region License
+// Copyright 2009-2010 Josh Close
+// This file is a part of CsvHelper and is licensed under the MS-PL
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html
+#endregion
+
+using System;
+
+namespace CsvHelper
+{
+	/// <summary>
+	/// Options for the CSV parser.
+	/// </summary>
+	public class CsvParserOptions
+	{
+		private int bufferSize = 2048;
+		private char delimiter = ',';
+
+		/// <summary>
+		/// The buffer size to use when
+		/// reading the stream.
+		/// </summary>
+		public int BufferSize
+		{
+			get { return bufferSize; } 
+			set { bufferSize = value; }
+		}
+
+		/// <summary>
+		/// The number of fields the CSV file has.
+		/// If this is known ahead of time, set
+		/// to make parsing more efficient.
+		/// </summary>
+		public int FieldCount { get; set; }
+
+		/// <summary>
+		/// The delimiter used to separate fields
+		/// of the CSV records.
+		/// </summary>
+		public char Delimiter
+		{
+			get { return delimiter; }
+			set
+			{
+				if( value == '\n' )
+				{
+					throw new Exception( "Newline is not a valid delimiter." );
+				}
+				delimiter = value;
+			}
+		}
+	}
+}
---
+++ src/CsvHelper/CsvPropertyDescriptorComparer.cs
@@ -0,0 +1,80 @@
+﻿#region License
+// Copyright 2009-2010 Josh Close
+// This file is a part of CsvHelper and is licensed under the MS-PL
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html
+#endregion
+using System;
+using System.Collections;
+using System.ComponentModel;
+
+namespace CsvHelper
+{
+	/// <summary>
+	/// Used to compare properties by <see cref="CsvFieldAttribute" />.
+	/// </summary>
+	public class CsvPropertyDescriptorComparer : IComparer
+	{
+		private readonly bool useFieldName;
+
+		/// <summary>
+		/// Creates a new instance of CsvFieldAttributeComparer.
+		/// </summary>
+		/// <param name="useFieldName">True to compare by <see cref="CsvFieldAttribute.FieldName" />, otherwise compares by <see cref="CsvFieldAttribute.FieldIndex" />.</param>
+		public CsvPropertyDescriptorComparer( bool useFieldName )
+		{
+			this.useFieldName = useFieldName;
+		}
+
+		/// <summary>
+		/// Compares two objects and returns a value indicating whether one is less than, equal to, or greater than the other.
+		/// </summary>
+		/// <returns>
+		/// Value 
+		///                     Condition 
+		///                     Less than zero 
+		///                 <paramref name="x"/> is less than <paramref name="y"/>. 
+		///                     Zero 
+		///                 <paramref name="x"/> equals <paramref name="y"/>. 
+		///                     Greater than zero 
+		///                 <paramref name="x"/> is greater than <paramref name="y"/>. 
+		/// </returns>
+		/// <param name="x">The first object to compare. 
+		///                 </param><param name="y">The second object to compare. 
+		///                 </param><exception cref="T:System.ArgumentException">Neither <paramref name="x"/> nor <paramref name="y"/> implements the <see cref="T:System.IComparable"/> interface.
+		///                     -or- 
+		///                 <paramref name="x"/> and <paramref name="y"/> are of different types and neither one can handle comparisons with the other. 
+		///                 </exception><filterpriority>2</filterpriority>
+		public int Compare( object x, object y )
+		{
+			var xProperty = x as PropertyDescriptor;
+			var yProperty = y as PropertyDescriptor;
+
+			if( xProperty == null )
+			{
+				throw new ArgumentNullException( "x" );
+			}
+			if( yProperty == null )
+			{
+				throw new ArgumentNullException( "y" );
+			}
+
+			var xAttribute = xProperty.Attributes[typeof( CsvFieldAttribute )] as CsvFieldAttribute;
+			var yAttribute = yProperty.Attributes[typeof( CsvFieldAttribute )] as CsvFieldAttribute;
+
+			if( xAttribute == null && yAttribute == null )
+			{
+				return 0;
+			}
+			if( xAttribute == null )
+			{
+				return 1;
+			}
+			if( yAttribute == null )
+			{
+				return -1;
+			}
+
+			return useFieldName ? xAttribute.FieldName.CompareTo( yAttribute.FieldName ) : xAttribute.FieldIndex.CompareTo( yAttribute.FieldIndex );
+		}
+	}
+}
