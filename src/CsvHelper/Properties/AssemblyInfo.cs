08aa549e8ee44e4b3a5933869e67f0798af33e0f
Josh Close <narshe@gmail.com>
2010-01-14 10:21:04 -0600

    Updated version number.

--- src/CsvHelper/Properties/AssemblyInfo.cs
+++ src/CsvHelper/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "0.3.0.*" )]
+[assembly: AssemblyVersion( "0.4.0.*" )]
 [assembly: AssemblyKeyFile( @"..\CsvHelper.snk" )]
