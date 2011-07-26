a2273ebb40c5016f7747c6b6b695cd7145644787
Valeriu Caraulean <caraulean@gmail.com>
2011-07-26 17:38:01 +0200

    Both CsvReader and CsvWriter are using Local culture when converting from/to strings.
    Modified and added tests that are working with a culture that have Decimal separator and DateTime format different than en-US or Invariant.

---
+++ src/CsvHelper.Tests/LocalCultureTests.cs
@@ -0,0 +1,99 @@
+﻿#region License
+// Copyright 2009-2011 Josh Close
+// This file is a part of CsvHelper and is licensed under the MS-PL
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html
+// http://csvhelper.com
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using CsvHelper.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsvHelper.Tests
+{
+	[TestClass]
+	public class LocalCultureTests
+	{
+		// In 'uk-UA' decimal separator is the ','
+		// For 'Invariant' and many other cultures decimal separator is '.'
+
+		[TestMethod]
+		public void ReadRecordsTest()
+		{
+			RunTestInSpecificCulture(ReadRecordsTest_Body, "uk-UA");
+		}
+
+		private static void ReadRecordsTest_Body()
+		{
+			const string source = "DecimalColumn;DateTimeColumn\r\n" + 
+			                      "12,0;11.11.2010\r\n";
+
+			var configuration = new CsvConfiguration 
+			{
+				Delimiter = ';', 
+			};
+			var reader = new CsvReader(new CsvParser(new StringReader(source), configuration), configuration);
+
+			var records = reader.GetRecords<TestRecordWithDecimal>().ToList();
+
+			Assert.AreEqual(1, records.Count());
+			var record = records.First();
+			Assert.AreEqual(12.0m, record.DecimalColumn);
+			Assert.AreEqual(new DateTime(2010, 11, 11), record.DateTimeColumn);
+		}
+
+		[TestMethod]
+		public void WriteRecordsTest()
+		{
+			RunTestInSpecificCulture(WriteRecordsTest_Body, "uk-UA");
+		}
+
+		private static void WriteRecordsTest_Body()
+		{
+			var records = new List<TestRecordWithDecimal>
+			{
+				new TestRecordWithDecimal
+				{                    
+					DecimalColumn = 12.0m,
+					DateTimeColumn = new DateTime(2010, 11, 11)
+				}                
+			};
+
+			var writer = new StringWriter();
+			var csv = new CsvWriter( writer, new CsvConfiguration {Delimiter = ';'});
+
+			csv.WriteRecords( records );
+
+			var csvFile = writer.ToString();
+
+			const string expected = "DecimalColumn;DateTimeColumn\r\n" + 
+			                        "12,0;11.11.2010\r\n";
+
+			Assert.AreEqual(expected, csvFile);
+		}
+
+		private static void RunTestInSpecificCulture(Action action, string cultureName)
+		{
+			var originalCulture = Thread.CurrentThread.CurrentCulture;
+			try
+			{
+				Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureName);
+				action();
+			}
+			finally
+			{
+				Thread.CurrentThread.CurrentCulture = originalCulture;
+			}
+		}
+
+		private class TestRecordWithDecimal
+		{
+			public decimal DecimalColumn { get; set; }
+			public DateTime DateTimeColumn { get; set; }
+		}
+	}
+}
