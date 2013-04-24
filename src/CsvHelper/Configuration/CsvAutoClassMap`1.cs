517b8b7571bb576f4951b02b3a2c69143c0e901c
Josh Close <narshe@gmail.com>
2013-04-23 23:12:02 -0500

    Updated version number. Updated copyright.

--- src/CsvHelper/Configuration/CsvAutoClassMap`1.cs
+++ src/CsvHelper/Configuration/CsvAutoClassMap`1.cs
@@ -1,4 +1,8 @@
-﻿namespace CsvHelper.Configuration
+﻿// Copyright 2009-2013 Josh Close
+// This file is a part of CsvHelper and is licensed under the MS-PL
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html
+// http://csvhelper.com
+namespace CsvHelper.Configuration
 {
 	/// <summary>
 	/// Mapping class that is created and used during auto mapping.
--- src/CsvHelper/Properties/AssemblyInfo.cs
+++ src/CsvHelper/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "1.17.0.*" )]
+[assembly: AssemblyVersion( "2.0.0.*" )]
