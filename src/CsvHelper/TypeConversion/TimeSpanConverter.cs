a7cef91346d8c4db9ce9b26e5711f1462a18a24c
Aravind Khasibhatla <khasibhatla@gmail.com>
2013-07-17 17:25:02 -0400

    Support for TimeSpan object
    Added support for TimeSpan, since the on the way in TimeSpan always came
    back as 00:00:00.000

---
+++ src/CsvHelper/TypeConversion/TimeSpanConverter.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2009-2013 Aravind Khasibhatla
+// This file is a part of CsvHelper and is licensed under the MS-PL
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html
+// http://csvhelper.com
+
+using System;
+using System.Globalization;
+
+namespace CsvHelper.TypeConversion
+{
+    /// <summary>
+    /// Converts a TimeSpan to and from a string.
+    /// </summary>
+    public class TimeSpanConverter : DefaultTypeConverter
+    {
+        /// <summary>
+        /// Determines whether this instance [can convert from] the specified type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>
+        ///   <c>true</c> if this instance [can convert from] the specified type; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool CanConvertFrom(Type type)
+        {
+            return type == typeof(string);
+        }
+
+        /// <summary>
+        /// Converts the string to an object.
+        /// </summary>
+        /// <param name="options">The options to use when converting.</param>
+        /// <param name="text">The string to convert to an object.</param>
+        /// <returns>The object created from the string.</returns>
+        public override object ConvertFromString(TypeConverterOptions options, string text)
+        {
+            var formatProvider = (IFormatProvider)options.CultureInfo.GetFormat(typeof(DateTimeFormatInfo)) ?? options.CultureInfo;
+
+            TimeSpan span;
+
+            if(TimeSpan.TryParse(text,formatProvider,out span))
+            {
+                return span;
+            }
+
+            return base.ConvertFromString(options, text);
+        }
+    }
+}
