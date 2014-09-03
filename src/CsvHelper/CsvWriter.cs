3d7b6f0c2113b4742528aa837bbef8ed8d5e7f44
Douglas Drinka <ddrinka@gmail.com>
2014-09-03 11:19:07 -0500

    Added non-generic CsvWriter methods
    Changed CsvWriter to match form of CsvReader with both generic and type-centric methods for reading/writing fields and records.

--- src/CsvHelper/CsvWriter.cs
+++ src/CsvHelper/CsvWriter.cs
@@ -168,13 +168,12 @@ namespace CsvHelper
 		/// <see cref="ICsvWriter.NextRecord" /> must be called
 		/// to complete writing of the current record.
 		/// </summary>
-		/// <typeparam name="T">The type of the field.</typeparam>
+		/// <param name="type">The type of the field.</param>
 		/// <param name="field">The field to write.</param>
-		public virtual void WriteField<T>( T field )
+		public virtual void WriteField( Type type, object field )
 		{
 			CheckDisposed();
 
-			var type = typeof( T );
 			if( type == typeof( string ) )
 			{
 				WriteField( field as string );
@@ -182,8 +181,46 @@ namespace CsvHelper
 			else
 			{
 				var converter = TypeConverterFactory.GetConverter( type );
-				WriteField( field, converter );
+				WriteField( type, field, converter );
 			}
+		}
+
+		/// <summary>
+		/// Writes the field to the CSV file.
+		/// When all fields are written for a record,
+		/// <see cref="ICsvWriter.NextRecord" /> must be called
+		/// to complete writing of the current record.
+		/// </summary>
+		/// <typeparam name="T">The type of the field.</typeparam>
+		/// <param name="field">The field to write.</param>
+		public virtual void WriteField<T>( T field )
+		{
+			CheckDisposed();
+
+			WriteField( typeof( T ), field );
+		}
+
+		/// <summary>
+		/// Writes the field to the CSV file.
+		/// When all fields are written for a record,
+		/// <see cref="ICsvWriter.NextRecord" /> must be called
+		/// to complete writing of the current record.
+		/// </summary>
+		/// <param name="type">The type of the field.</param>
+		/// <param name="field">The field to write.</param>
+		/// <param name="converter">The converter used to convert the field into a string.</param>
+		public virtual void WriteField( Type type, object field, ITypeConverter converter )
+		{
+			CheckDisposed();
+
+			var typeConverterOptions = TypeConverterOptionsFactory.GetOptions( type );
+			if( typeConverterOptions.CultureInfo == null )
+			{
+				typeConverterOptions.CultureInfo = configuration.CultureInfo;
+			}
+
+			var fieldString = converter.ConvertToString( typeConverterOptions, field );
+			WriteField( fieldString );
 		}
 
 		/// <summary>
@@ -199,14 +236,7 @@ namespace CsvHelper
 		{
 			CheckDisposed();
 
-			var typeConverterOptions = TypeConverterOptionsFactory.GetOptions<T>();
-			if( typeConverterOptions.CultureInfo == null )
-			{
-				typeConverterOptions.CultureInfo = configuration.CultureInfo;
-			}
-
-			var fieldString = converter.ConvertToString( typeConverterOptions, field );
-			WriteField( fieldString );
+			WriteField( typeof( T ), field, converter );
 		}
 
 		/// <summary>
@@ -326,25 +356,37 @@ namespace CsvHelper
 		/// <summary>
 		/// Writes the record to the CSV file.
 		/// </summary>
-		/// <typeparam name="T">The type of the record.</typeparam>
+		/// <param name="type">The type of the record.</typeparam>
 		/// <param name="record">The record to write.</param>
-		public virtual void WriteRecord<T>( T record )
+		public virtual void WriteRecord( Type type, object record )
 		{
 			CheckDisposed();
 
 			try
 			{
-				GetWriteRecordAction<T>()( record );
+				GetWriteRecordAction( type ).DynamicInvoke( record );
 			}
 			catch( Exception ex )
 			{
-				ExceptionHelper.AddExceptionDataMessage( ex, null, typeof( T ), null, null, null );
+				ExceptionHelper.AddExceptionDataMessage( ex, null, type, null, null, null );
 				throw;
 			}
 
 			hasRecordBeenWritten = true;
 
 			NextRecord();
+		}
+
+		/// <summary>
+		/// Writes the record to the CSV file.
+		/// </summary>
+		/// <typeparam name="T">The type of the record.</typeparam>
+		/// <param name="record">The record to write.</param>
+		public virtual void WriteRecord<T>( T record )
+		{
+			CheckDisposed();
+
+			WriteRecord( typeof( T ), record );
 		}
 
 		/// <summary>
