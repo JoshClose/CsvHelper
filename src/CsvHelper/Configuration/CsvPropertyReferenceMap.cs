ad785210aaa33711cb46d3d5596419d0c5c6e00f
Phil Lee <phil.lee.007@gmail.com>
2014-10-20 20:11:59 +1300

    Feature - Reference prefixes when mapping #270
    Added .Prefix() method to CsvPropertyReferenceMap so that reference
    properties could have their fields prefixed if needed.
    
    Note: Initially I used the same style as for PrefixReferenceHeaders in
    CsvConfiguration, using the format [Prefix].[FieldName]. However, I
    removed the '.' separator as this made it less flexible than simply
    allowing the user to determine their own prefix scheme

--- src/CsvHelper/Configuration/CsvPropertyReferenceMap.cs
+++ src/CsvHelper/Configuration/CsvPropertyReferenceMap.cs
@@ -27,6 +27,11 @@ namespace CsvHelper.Configuration
 		/// </summary>
 		public CsvClassMap Mapping { get; protected set; }
 
+        /// <summary>
+        /// Gets the prefix for the reference property
+        /// </summary>
+        public string FieldPrefix { get; protected set; }
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="CsvPropertyReferenceMap"/> class.
 		/// </summary>
@@ -42,6 +47,36 @@ namespace CsvHelper.Configuration
 			this.property = property;
 			Mapping = mapping;
 		}
+
+        /// <summary>
+        /// Appends a prefix to the header of each field of the reference property
+        /// </summary>
+        /// <param name="prefix">The prefix to be prepended to headers of each reference property</param>
+        /// <returns>The current <see cref="CsvPropertyReferenceMap" /></returns>
+        public CsvPropertyReferenceMap Prefix(string prefix)
+        {
+            if (String.IsNullOrEmpty(prefix))
+            {
+                throw new ArgumentNullException("prefix");
+            }
+
+            if (!String.IsNullOrEmpty(FieldPrefix))
+            {
+                throw new InvalidOperationException(String.Format("Prefix has already been set to {0}. You can only set the prefix once.", FieldPrefix));
+            }
+
+            FieldPrefix = prefix;
+
+            foreach (var propertyMap in Mapping.PropertyMaps)
+            {
+                for (var i = 0; i < propertyMap.Data.Names.Count; i++)
+                {
+                    propertyMap.Data.Names[i] = string.Format("{0}{1}", prefix, propertyMap.Data.Names[i]);
+                }
+            }
+
+            return this;
+        }
 
 		/// <summary>
 		/// Get the largest index for the
