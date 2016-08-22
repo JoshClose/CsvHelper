2206c2d391bf5851f3654a3fc3b79ab6dd6ab407
Josh Close <narshe@gmail.com>
2016-08-22 13:00:24 -0500

    Starting 3.0 work. Bumping beta version.

--- src/CsvHelper/Properties/AssemblyInfo.cs
+++ src/CsvHelper/Properties/AssemblyInfo.cs
@@ -9,14 +9,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "CsvHelper" )]
-[assembly: AssemblyDescription( "A library for reading and writing CSV files. Extremely fast, flexible, and easy to use. Supports reading and writing of custom class objects." )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Josh Close" )]
 [assembly: AssemblyProduct( "CsvHelper" )]
-[assembly: AssemblyCopyright( "Copyright © Josh Close 2009-2016" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
+//[assembly: AssemblyTitle( "CsvHelper" )]
+//[assembly: AssemblyCompany( "Josh Close" )]
+//[assembly: AssemblyDescription( "A library for reading and writing CSV files. Extremely fast, flexible, and easy to use. Supports reading and writing of custom class objects." )]
+//[assembly: AssemblyConfiguration( "" )]
+//[assembly: AssemblyCopyright( "Copyright © Josh Close 2009-2016" )]
+//[assembly: AssemblyTrademark( "" )]
+//[assembly: AssemblyCulture( "" )]
 
 #if !PCL
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -39,10 +39,11 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 
 // DO NOT update this.
-[assembly: AssemblyVersion( "2.0.0.0" )]
+[assembly: AssemblyVersion( "3.0.0.0" )]
 
+// This is now set from the project.json file.
 // Update this.
-[assembly: AssemblyFileVersion( "2.16.0.0" )]
+//[assembly: AssemblyFileVersion( "2.16.0.0" )]
 
 [assembly: InternalsVisibleTo( "CsvHelper.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000db97564beef98ad18a76ba31f769fab92b14341c9c37ed12f8004bb2a1a7fe42ad829b0e285915a816f05a32325c5e0ba83bd69d8f4d26a0785ccf446749842ad038f7325601a99c59a323dfa7ecf210139159da0aad1822b5d9c9be6d914ecbaa8b8c908c4af798a89b8777010971d81975079a49662ced398c742ff186a94" )]
 [assembly: InternalsVisibleTo( "CsvHelper20.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000db97564beef98ad18a76ba31f769fab92b14341c9c37ed12f8004bb2a1a7fe42ad829b0e285915a816f05a32325c5e0ba83bd69d8f4d26a0785ccf446749842ad038f7325601a99c59a323dfa7ecf210139159da0aad1822b5d9c9be6d914ecbaa8b8c908c4af798a89b8777010971d81975079a49662ced398c742ff186a94" )]
