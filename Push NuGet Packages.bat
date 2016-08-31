aae105a06084f09e5ce79e228df39066ca83e7e4
Josh Close <narshe@gmail.com>
2016-08-31 00:37:38 -0500

    Updated pushed to require it be done via the command line.

--- Push NuGet Packages.bat
+++ Push NuGet Packages.bat
@@ -1,3 +1,15 @@
-.\src\.nuget\NuGet.exe push .\NuGet\CsvHelper.*.symbols.nupkg -Source https://www.nuget.org/api/v2/package
+:: .\src\.nuget\NuGet.exe push .\NuGet\CsvHelper.*.symbols.nupkg -Source https://www.nuget.org/api/v2/package
 
-pause
+@echo off
+
+if [%1]==[] goto USAGE
+
+.\src\.nuget\NuGet.exe push %1 -source nuget.org
+
+goto DONE
+
+:USAGE
+
+@echo Usage: %0 ^<package.nupkg^>
+
+:DONE
