4a5eb5c25e75d6bb56ac1e41a245d90ffec8d66b
jamesbascle <rbascle9@gmail.com>
2016-10-07 11:50:05 -0400

    Adding ClassMapBuilder.

---
+++ src/CsvHelper.Tests/Configuration/ClassMapBuilderTests.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsvHelper.Tests.Configuration
+{
+    [TestClass]
+    public class ClassMapBuilderTests
+    {
+        private static readonly CsvFactory csvFactory = new CsvFactory();
+
+        private static Func<ICsvReaderRow, FakeInnerClass> ConvertExpression => r => new FakeInnerClass { E = r.GetField(4)};
+
+        private static readonly CsvClassMap<FakeClass> map = csvFactory
+            .Map<FakeClass>(m=>m.A).Name("A1").NameIndex(2).Default("WEW")
+            .Map(m=>m.B).Name("B2").Default(2)
+            .Map(m=>m.C).Index(2).TypeConvert(new DateTimeConverter())
+            .Map(m => m.D).Name("D4").TypeConvert<DoubleConverter>().Default(4d)
+            .Map(m => m.E).ConvertUsing(ConvertExpression)
+            .Build();
+
+        [TestMethod]
+        public void ClassMapBuilderAddsPropertyMapsCorrectly()
+        {
+            Assert.AreEqual(5, map.PropertyMaps.Count );//IMappable
+        }
+
+        [TestMethod]
+        public void ClassMapBuilderAddsTypeConvertersCorrectly()
+        {
+            Assert.AreEqual(typeof(DateTimeConverter), map.PropertyMaps[2].Data.TypeConverter.GetType());//2
+            Assert.AreEqual(typeof(DoubleConverter), map.PropertyMaps[3].Data.TypeConverter.GetType());//2
+        }
+
+        [TestMethod]
+        public void ClassMapBuilderAddsIndexesCorrectly()
+        {
+            Assert.AreEqual( 2, map.PropertyMaps[2].Data.Index ); //3
+        }
+
+        [TestMethod]
+        public void ClassMapBuilderAddsNamesCorrectly()
+        {
+            Assert.AreEqual( "D4", map.PropertyMaps[3].Data.Names.Single() ); //4
+        }
+
+        [TestMethod]
+        public void ClassMapBuilderAddsNameIndexesCorrectly()
+        {
+            Assert.AreEqual( 2, map.PropertyMaps[0].Data.NameIndex ); //5
+        }
+
+        //this one is kind of hacky, but i'm not sure how else to test it more robustly since the function gets converted to an expression inside the CsvClassMap
+        [TestMethod]
+        public void ClassMapBuilderAddsConvertUsingFunctionCorectly()
+        {
+            var fakeRow = new BuilderRowFake();
+            Assert.AreEqual( ConvertExpression( fakeRow ).E, ( map.PropertyMaps[4].Data.ConvertExpression as Expression<Func<ICsvReaderRow, FakeInnerClass>> ).Compile()( fakeRow ).E ); //6
+        }
+
+        [TestMethod]
+        public void ClassMapBuilderAddsDefaultsCorrectly()
+        {
+            Assert.AreEqual("WEW", map.PropertyMaps[0].Data.Default);//7
+            Assert.AreEqual(4d, map.PropertyMaps[3].Data.Default);//7
+        }
+
+        private class BuilderRowFake : ICsvReaderRow
+        {
+            public ICsvReaderConfiguration Configuration { get; }
+            public string[] FieldHeaders { get; }
+            public string[] CurrentRecord { get; }
+            public int Row { get; }
+
+            string ICsvReaderRow.this[ int index ]
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            string ICsvReaderRow.this[ string name ]
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            string ICsvReaderRow.this[ string name, int index ]
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public string GetField( int index )
+            {
+                return index.ToString();
+            }
+
+            public string GetField( string name )
+            {
+                throw new NotImplementedException();
+            }
+
+            public string GetField( string name, int index )
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetField( Type type, int index )
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetField( Type type, string name )
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetField( Type type, string name, int index )
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetField( Type type, int index, ITypeConverter converter )
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetField( Type type, string name, ITypeConverter converter )
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetField( Type type, string name, int index, ITypeConverter converter )
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetField<T>( int index )
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetField<T>( string name )
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetField<T>( string name, int index )
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetField<T>( int index, ITypeConverter converter )
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetField<T>( string name, ITypeConverter converter )
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetField<T>( string name, int index, ITypeConverter converter )
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetField<T, TConverter>( int index ) where TConverter : ITypeConverter
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetField<T, TConverter>( string name ) where TConverter : ITypeConverter
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetField<T, TConverter>( string name, int index ) where TConverter : ITypeConverter
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField( Type type, int index, out object field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField( Type type, string name, out object field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField( Type type, string name, int index, out object field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField( Type type, int index, ITypeConverter converter, out object field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField( Type type, string name, ITypeConverter converter, out object field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField( Type type, string name, int index, ITypeConverter converter, out object field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField<T>( int index, out T field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField<T>( string name, out T field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField<T>( string name, int index, out T field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField<T>( int index, ITypeConverter converter, out T field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField<T>( string name, ITypeConverter converter, out T field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField<T>( string name, int index, ITypeConverter converter, out T field )
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField<T, TConverter>( int index, out T field ) where TConverter : ITypeConverter
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField<T, TConverter>( string name, out T field ) where TConverter : ITypeConverter
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetField<T, TConverter>( string name, int index, out T field ) where TConverter : ITypeConverter
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsRecordEmpty()
+            {
+                throw new NotImplementedException();
+            }
+
+            public T GetRecord<T>()
+            {
+                throw new NotImplementedException();
+            }
+
+            public object GetRecord( Type type )
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+    }
+
+
+    public class FakeClass
+    {
+        public string A { get; set; }
+        public int B { get; set; }
+        public DateTime C { get; set; }
+        public double D { get; set; }
+        public FakeInnerClass E {get; set;}
+    }
+
+    public class FakeInnerClass
+    {
+        public string E { get; set; }
+    }
+}
---
+++ src/CsvHelper/Configuration/ClassMapBuilder.cs
@@ -0,0 +1,312 @@
+﻿// Copyright 2009-2015 Josh Close and Contributors
+// This file is a part of CsvHelper and is dual licensed under MS-PL and Apache 2.0.
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html for MS-PL and http://opensource.org/licenses/Apache-2.0 for Apache 2.0.
+// http://csvhelper.com
+
+
+#if !NET_2_0
+using System;
+using System.Linq.Expressions;
+using CsvHelper.TypeConversion;
+
+namespace CsvHelper.Configuration
+{
+    
+    /// <summary>
+    /// Entry point for fluently mapping a property
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <remarks>Node 1</remarks>
+    public interface IMappable<T> : IBuildable<T>
+    {
+        ///<summary>
+		/// Maps a property to a CSV field.
+		/// </summary>
+		/// <param name="expression">The property to map.</param>
+		/// <param name="useExistingMap">If true, an existing map will be used if available.
+		/// If false, a new map is created for the same property.</param>
+		/// <returns>Remaining available options</returns>
+        /// <remarks><see cref="CsvClassMap{T}.Map(Expression{Func{T, object}}, bool)"/> </remarks>
+        IMappedOptions<T> Map(Expression<Func<T, object>> expression, bool useExistingMap = true);
+    }
+
+    /// <summary>
+    /// Options after initial property map.
+    /// </summary>
+    /// <typeparam name="T">Type to Map</typeparam>
+    /// <remarks>Edges from Node 1 => 1,2,3,4,6,7</remarks>
+    public interface IMappedOptions<T> 
+        : IMappable<T>, //1
+        ITypeConvertible<T>, //2
+        IIndexable<T>, //3
+        INameable<T>, //4
+        IConvertUsingable<T>, //6
+        IDefaultable<T> //7 
+    { }
+
+    /// <summary>
+    /// Node to represent ability to call <see cref="TypeConvert(ITypeConverter)"/> 
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Node 2</remarks>
+    public interface ITypeConvertible<T> : IBuildable<T>
+    {
+        /// <summary>
+		/// Specifies the <see cref="ITypeConverter"/> to use
+		/// when converting the property to and from a CSV field.
+		/// </summary>
+		/// <param name="typeConverter">The TypeConverter to use.</param>
+		/// <returns>Remaining available options</returns>
+		/// <remarks><see cref="CsvPropertyMap.TypeConverter(ITypeConverter)"/> </remarks>
+        ITypeConvertedOptions<T> TypeConvert(ITypeConverter typeConverter);
+
+        /// <summary>
+        /// Specifies the <see cref="ITypeConverter"/> to use
+        /// when converting the property to and from a CSV field.
+        /// </summary>
+        /// <typeparam name="TConverter">The <see cref="Type"/> of the 
+        /// <see cref="ITypeConverter"/> to use.</typeparam>
+        /// <remarks><see cref="CsvPropertyMap.TypeConverter{T}()"/> </remarks>
+        ITypeConvertedOptions<T> TypeConvert<TConverter>() where TConverter : ITypeConverter;
+    }
+    
+    /// <summary>
+    /// Options available after calling <see cref="ITypeConvertible{T}.TypeConvert(ITypeConverter)"/> .
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Edges from Node 2 => 1,7</remarks>
+    public interface ITypeConvertedOptions<T> : 
+        IMappable<T>, //1
+        IDefaultable<T> //7
+    { }
+
+    /// <summary>
+    /// Node to represent ability to call <see cref="Index(int)"/> 
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Node 3</remarks>
+    /// 
+    public interface IIndexable<T> : IBuildable<T>
+    {
+        /// <summary>
+        /// When reading, is used to get the field at
+        /// the given index. When writing, the fields
+        /// will be written in the order of the field
+        /// indexes.
+        /// </summary>
+        /// <param name="index">The index of the CSV field.</param>
+        /// <returns>Remaining available options</returns>
+        /// <remarks><see cref="CsvPropertyMap.Index(int, int)"/> </remarks>
+        IIndexedOptions<T> Index(int index);
+    }
+
+    /// <summary>
+    /// Options available after calling <see cref="IIndexable{T}.Index(int)"/> .
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Edges from Node 3=>1,2,7</remarks>
+    public interface IIndexedOptions<T> :
+        IMappable<T>, //1
+        ITypeConvertible<T>, //2
+        IDefaultable<T> //7
+    { }
+
+    /// <summary>
+    /// Node to represent ability to call <see cref="Name(string[])"/> 
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Node 4</remarks>
+    public interface INameable<T> : IBuildable<T>
+    {
+        /// <summary>
+        /// When reading, is used to get the field
+        /// at the index of the name if there was a
+        /// header specified. It will look for the
+        /// first name match in the order listed.
+        /// When writing, sets the name of the 
+        /// field in the header record.
+        /// The first name will be used.
+        /// </summary>
+        /// <param name="names">The possible names of the CSV field.</param>
+        /// <returns>Remaining available options</returns>
+        /// <remarks><see cref="CsvPropertyMap.Name(string[])"/></remarks>
+        INamedOptions<T> Name(params string[] names);
+    }
+
+    /// <summary>
+    /// Options available after calling <see cref="INameable{T}.Name(string[])"/> .
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Edges from Node 4=>1,2,5,7</remarks>
+    public interface INamedOptions<T> :
+        IMappable<T>, //1
+        ITypeConvertible<T>, //2
+        INameIndexable<T>, //5
+        IDefaultable<T> //7
+    { }
+
+
+    /// <summary>
+    /// Node to represent ability to call <see cref="NameIndex(int)"/> 
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Node 5</remarks>
+    public interface INameIndexable<T> : IBuildable<T>
+    {
+        /// <summary>
+        /// When reading, is used to get the 
+        /// index of the name used when there 
+        /// are multiple names that are the same.
+        /// </summary>
+        /// <param name="index">The index of the name.</param>
+        /// <returns>Remaining available options</returns>
+        /// <remarks><see cref="CsvPropertyMap.NameIndex(int)"/></remarks>
+        INameIndexedOptions<T> NameIndex(int index);
+    }
+
+    /// <summary>
+    /// Options available after calling <see cref="INameIndexable{T}.NameIndex(int)"/> .
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Edges from Node 5=>1,2,7</remarks>
+    public interface INameIndexedOptions<T> :
+        IMappable<T>, //1
+        ITypeConvertible<T>, //2
+        IDefaultable<T> //7
+    { }
+
+    /// <summary>
+    /// Node to represent ability to call <see cref="ConvertUsing{TProperty}(Func{ICsvReaderRow, TProperty})"/> 
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Node 6</remarks>
+    public interface IConvertUsingable<T> : IBuildable<T>
+    {
+        /// <summary>
+        /// Specifies an expression to be used to convert data in the
+        /// row to the property.
+        /// </summary>
+        /// <typeparam name="TProperty">The type of the property that will be set.</typeparam>
+        /// <param name="convertExpression">The convert expression.</param>
+        /// <returns>Remaining available options</returns>
+        /// <remarks><see cref="CsvPropertyMap.ConvertUsing{T}(Func{ICsvReaderRow, T})"/></remarks>
+        IMappable<T> ConvertUsing<TProperty>(Func<ICsvReaderRow, TProperty> convertExpression);
+    }
+
+    /// <summary>
+    /// Node to represent ability to call <see cref="Default{TProperty}(TProperty)"/> 
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Node 7</remarks>
+    public interface IDefaultable<T> : IBuildable<T>
+    {
+        /// <summary>
+        /// The default value that will be used when reading when
+        /// the CSV field is empty.
+        /// </summary>
+        /// <typeparam name="TProperty">The default type.</typeparam>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>Remaining available options</returns>
+        /// <remarks><seealso cref="CsvPropertyMap.Default{T}(T)"/></remarks>
+        IMappable<T> Default<TProperty>(TProperty defaultValue);
+    }
+
+    /// <summary>
+    /// Node to represent ability to call <see cref="Build()"/> 
+    /// </summary>
+    /// <typeparam name="T">Type being Mapped</typeparam>
+    /// <remarks>Not represented as a node, since at the moment, all nodes are marked as <see cref="IBuildable{T}"/></remarks>
+    public interface IBuildable<T>
+    {
+        /// <summary>
+        /// Builds the configured <see cref="CsvClassMap{T}"/>
+        /// </summary>
+        /// <returns>The built <see cref="CsvClassMap{T}"/></returns>
+        CsvClassMap<T> Build();
+    }
+
+    internal interface IAllable<T> :
+        IMappable<T>, //1
+        IMappedOptions<T>, //1 result
+        ITypeConvertible<T>, //2
+        ITypeConvertedOptions<T>,//2 result
+        IIndexable<T>, //3
+        IIndexedOptions<T>, //3 result
+        INameable<T>, //4
+        INamedOptions<T>, //4 result
+        INameIndexable<T>, //5
+        INameIndexedOptions<T>, //5 result
+        IConvertUsingable<T>, //6 - goes back to 1 only
+        IDefaultable<T> //7 - goes back to 1 only
+    { }
+
+    internal class ClassMapBuilder<TClass> : IAllable<TClass>
+    {
+        private CsvPropertyMap currPropertyMap;
+
+        private readonly CsvClassMap<TClass> currClassMap;
+
+        public ClassMapBuilder()
+        {
+            currClassMap = new BuilderClassMap<TClass>();
+        }
+
+        private sealed class BuilderClassMap<T> : CsvClassMap<T>{}
+
+        public IMappedOptions<TClass> Map(Expression<Func<TClass, object>> expression, bool useExistingMap = true)
+        {
+            currPropertyMap = currClassMap.Map(expression, useExistingMap);
+            return this;   
+        }
+
+        public ITypeConvertedOptions<TClass> TypeConvert(ITypeConverter typeConverter)
+        {
+            currPropertyMap.TypeConverter( typeConverter );
+            return this;
+        }
+
+        public ITypeConvertedOptions<TClass> TypeConvert<TConverter>() where TConverter : ITypeConverter
+        {
+            currPropertyMap.TypeConverter<TConverter>();
+            return this;
+        }
+
+        public IIndexedOptions<TClass> Index(int i)
+        {
+            currPropertyMap.NameIndex(i);
+            return this;
+        }
+
+        public INamedOptions<TClass> Name(params string[] n)
+        {
+            currPropertyMap.Name( n );
+            return this;
+        }
+
+        public INameIndexedOptions<TClass> NameIndex(int ni)
+        {
+            currPropertyMap.NameIndex(ni);
+            return this;
+        }
+
+        public IMappable<TClass> ConvertUsing<TProperty>(Func<ICsvReaderRow, TProperty> t)
+        {
+            currPropertyMap.ConvertUsing( t );
+            return this;
+        }
+
+        public IMappable<TClass> Default<TProperty>(TProperty defaultValue)
+        {
+            currPropertyMap.Default( defaultValue );
+            return this;
+        }
+
+        public CsvClassMap<TClass> Build()
+        {
+            return currClassMap;
+        }
+    }
+
+    
+}
+#endif
