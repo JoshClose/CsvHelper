1165e691e2b0f8f7ff17f6f36b4aea4af0f8fe0a
Josh Close <j_close@crgl-thirdparty.com>
2017-09-05 21:28:44 -0500

    Version bump.

--- src/CsvHelper/CsvFactory.cs
+++ src/CsvHelper/CsvFactory.cs
@@ -89,7 +89,6 @@ namespace CsvHelper
         /// Access point for fluent interface to dynamically build a <see cref="CsvClassMap{T}"/>
         /// </summary>
         /// <typeparam name="T">Type you will be making a class map for</typeparam>
-        /// <param name="map">First property of <typeparamref name="T"/> you will be mapping</param>
         /// <returns>Options to further configure the <see cref="CsvClassMap{T}"/></returns>
         public IHasMap<T> CreateClassMapBuilder<T>()
 	    {
