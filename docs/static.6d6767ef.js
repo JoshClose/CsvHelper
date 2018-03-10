504b2e227e8eb7ce37da733c55fa600ca99f8980
Josh Close <narshe@gmail.com>
2018-03-10 00:15:27 -0600

    Updated docs.

--- docs/static.6d6767ef.js
+++ docs/static.6d6767ef.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://joshclose.github.io/CsvHelper/";
+/******/ 	__webpack_require__.p = "https://joshclose.github.io/CsvHelper/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 7);
@@ -1646,4 +1646,4 @@ exports.default = Header;
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.ad5953b3.js.map
+//# sourceMappingURL=static.6d6767ef.js.map
