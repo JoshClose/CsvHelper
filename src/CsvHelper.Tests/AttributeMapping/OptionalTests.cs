c80392a2a54ce585971f948f6066664f440ea4f2
Josh Close <narshe@gmail.com>
2019-01-03 12:42:45 -0600

    Updated license header files.

--- src/CsvHelper.Tests/AttributeMapping/OptionalTests.cs
+++ src/CsvHelper.Tests/AttributeMapping/OptionalTests.cs
@@ -1,4 +1,8 @@
-﻿using CsvHelper.Configuration.Attributes;
+﻿// Copyright 2009-2019 Josh Close and Contributors
+// This file is a part of CsvHelper and is dual licensed under MS-PL and Apache 2.0.
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html for MS-PL and http://opensource.org/licenses/Apache-2.0 for Apache 2.0.
+// https://github.com/JoshClose/CsvHelper
+using CsvHelper.Configuration.Attributes;
 using CsvHelper.Tests.Mocks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
