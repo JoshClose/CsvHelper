9a0d661f8879438733b68397908e0e10d432f577
Josh Close <narshe@gmail.com>
2019-10-28 10:30:53 -0500

    Created net47 build that doesn't include System.ValueTuple.

--- build/Build.cs
+++ build/Build.cs
@@ -24,7 +24,7 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main() => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Test);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
