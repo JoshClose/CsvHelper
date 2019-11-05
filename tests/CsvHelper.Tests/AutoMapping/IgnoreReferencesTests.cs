4047fc9359b05648d2cb92994cfa1cdd5c39b065
Josh Close <narshe@gmail.com>
2019-11-05 12:19:48 -0600

    Added unit tests for ignore references configuration.

---
+++ tests/CsvHelper.Tests/AutoMapping/IgnoreReferencesTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvHelper.Tests.AutoMapping
+{
+	[TestClass]
+	public class IgnoreReferencesTests
+	{
+		[TestMethod]
+		public void IgnoreReferncesWritingTest()
+		{
+			var records = new List<Foo>
+			{
+				new Foo
+				{
+					Id = 1,
+					Bar = new Bar { Name = "one" }
+				}
+			};
+
+			using (var writer = new StringWriter())
+			using (var csv = new CsvWriter(writer))
+			{
+				csv.Configuration.IgnoreReferences = true;
+				csv.WriteRecords(records);
+				writer.Flush();
+
+				var expected = new StringBuilder();
+				expected.AppendLine("Id");
+				expected.AppendLine("1");
+
+				Assert.AreEqual(expected.ToString(), writer.ToString());
+			}
+		}
+
+		private class Foo
+		{
+			public int Id { get; set; }
+
+			public Bar Bar { get; set; }
+		}
+
+		private class Bar
+		{
+			public string Name { get; set; }
+		}
+	}
+}
