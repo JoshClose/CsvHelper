4cf576ec5524f73fc7894232b23a5969acc48e46
Josh Close <narshe@gmail.com>
2020-01-09 13:02:33 -0600

    Updated license header for 2020.

--- tests/CsvHelper.Tests/Parsing/CommentTests.cs
+++ tests/CsvHelper.Tests/Parsing/CommentTests.cs
@@ -1,8 +1,7 @@
-﻿// Copyright 2009-2019 Josh Close and Contributors
+﻿// Copyright 2009-2020 Josh Close and Contributors
 // This file is a part of CsvHelper and is dual licensed under MS-PL and Apache 2.0.
 // See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html for MS-PL and http://opensource.org/licenses/Apache-2.0 for Apache 2.0.
 // https://github.com/JoshClose/CsvHelper
-
 using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
