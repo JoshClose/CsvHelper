f60df92faf89fcb66157b70d491b0a205ca4be6b
Josh Close <narshe@gmail.com>
2020-01-09 22:36:46 -0600

    Changed double and single converters to only test for format "R" if the user hasn't supplied a format.

--- src/CsvHelper/TypeConversion/DoubleConverter.cs
+++ src/CsvHelper/TypeConversion/DoubleConverter.cs
@@ -2,70 +2,56 @@
 // This file is a part of CsvHelper and is dual licensed under MS-PL and Apache 2.0.
 // See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html for MS-PL and http://opensource.org/licenses/Apache-2.0 for Apache 2.0.
 // https://github.com/JoshClose/CsvHelper
+using System;
 using System.Globalization;
 using System.Linq;
 using CsvHelper.Configuration;
 
 namespace CsvHelper.TypeConversion
 {
-    /// <summary>
-    /// Converts a <see cref="double"/> to and from a <see cref="string"/>.
-    /// </summary>
-    public class DoubleConverter : DefaultTypeConverter
-    {
-		private string defaultFormat;
+	/// <summary>
+	/// Converts a <see cref="double"/> to and from a <see cref="string"/>.
+	/// </summary>
+	public class DoubleConverter : DefaultTypeConverter
+	{
+		private Lazy<string> defaultFormat = new Lazy<string>(() => double.TryParse(double.MaxValue.ToString("R"), out var _) ? "R" : "G17");
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="DoubleConverter"/> class.
+		/// Converts the object to a string.
 		/// </summary>
-		public DoubleConverter()
+		/// <param name="value">The object to convert to a string.</param>
+		/// <param name="row">The <see cref="IWriterRow"/> for the current record.</param>
+		/// <param name="memberMapData">The <see cref="MemberMapData"/> for the member being written.</param>
+		/// <returns>The string representation of the object.</returns>
+		public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
 		{
-			// Have to use a try/catch here because of a bug in .NET that causes a StackOverflowException when calling ToString("R").
-			try
+			var format = memberMapData.TypeConverterOptions.Formats?.FirstOrDefault() ?? defaultFormat.Value;
+
+			if (value is double d)
 			{
-				double.Parse(double.MaxValue.ToString("R"));
-				defaultFormat = "R";
+				return d.ToString(format, memberMapData.TypeConverterOptions.CultureInfo);
 			}
-			catch
-			{
-				defaultFormat = "G17";
-			}
+
+			return base.ConvertToString(value, row, memberMapData);
 		}
 
-        /// <summary>
-        /// Converts the object to a string.
-        /// </summary>
-        /// <param name="value">The object to convert to a string.</param>
-        /// <param name="row">The <see cref="IWriterRow"/> for the current record.</param>
-        /// <param name="memberMapData">The <see cref="MemberMapData"/> for the member being written.</param>
-        /// <returns>The string representation of the object.</returns>
-        public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
-        {
-			if (value is double d && memberMapData.TypeConverterOptions.Formats?.FirstOrDefault() == null)
-            {
-                return d.ToString(defaultFormat, memberMapData.TypeConverterOptions.CultureInfo);
-            }
+		/// <summary>
+		/// Converts the string to an object.
+		/// </summary>
+		/// <param name="text">The string to convert to an object.</param>
+		/// <param name="row">The <see cref="IReaderRow"/> for the current record.</param>
+		/// <param name="memberMapData">The <see cref="MemberMapData"/> for the member being created.</param>
+		/// <returns>The object created from the string.</returns>
+		public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+		{
+			var numberStyle = memberMapData.TypeConverterOptions.NumberStyle ?? NumberStyles.Float;
 
-            return base.ConvertToString(value, row, memberMapData);
-        }
+			if (double.TryParse(text, numberStyle, memberMapData.TypeConverterOptions.CultureInfo, out var d))
+			{
+				return d;
+			}
 
-        /// <summary>
-        /// Converts the string to an object.
-        /// </summary>
-        /// <param name="text">The string to convert to an object.</param>
-        /// <param name="row">The <see cref="IReaderRow"/> for the current record.</param>
-        /// <param name="memberMapData">The <see cref="MemberMapData"/> for the member being created.</param>
-        /// <returns>The object created from the string.</returns>
-        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
-        {
-            var numberStyle = memberMapData.TypeConverterOptions.NumberStyle ?? NumberStyles.Float;
-
-            if (double.TryParse(text, numberStyle, memberMapData.TypeConverterOptions.CultureInfo, out var d))
-            {
-                return d;
-            }
-
-            return base.ConvertFromString(text, row, memberMapData);
-        }
-    }
+			return base.ConvertFromString(text, row, memberMapData);
+		}
+	}
 }
--- src/CsvHelper/TypeConversion/SingleConverter.cs
+++ src/CsvHelper/TypeConversion/SingleConverter.cs
@@ -14,59 +14,44 @@ namespace CsvHelper.TypeConversion
 	/// </summary>
 	public class SingleConverter : DefaultTypeConverter
 	{
-		private string defaultFormat;
+		private Lazy<string> defaultFormat = new Lazy<string>(() => float.TryParse(float.MaxValue.ToString("R"), out var _) ? "R" : "G9");
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="SingleConverter"/> class.
+		/// Converts the object to a string.
 		/// </summary>
-		public SingleConverter()
+		/// <param name="value">The object to convert to a string.</param>
+		/// <param name="row">The <see cref="IWriterRow"/> for the current record.</param>
+		/// <param name="memberMapData">The <see cref="MemberMapData"/> for the member being written.</param>
+		/// <returns>The string representation of the object.</returns>
+		public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
 		{
-			// Have to use a try/catch here because of a bug in .NET that causes a StackOverflowException when calling ToString("R").
-			try
+			var format = memberMapData.TypeConverterOptions.Formats?.FirstOrDefault() ?? defaultFormat.Value;
+
+			if (value is float f)
 			{
-				float.Parse(float.MaxValue.ToString("R"));
-				defaultFormat = "R";
+				return f.ToString(format, memberMapData.TypeConverterOptions.CultureInfo);
 			}
-			catch
-			{
-				defaultFormat = "G9";
-			}
+
+			return base.ConvertToString(value, row, memberMapData);
 		}
 
-        /// <summary>
-        /// Converts the object to a string.
-        /// </summary>
-        /// <param name="value">The object to convert to a string.</param>
-        /// <param name="row">The <see cref="IWriterRow"/> for the current record.</param>
-        /// <param name="memberMapData">The <see cref="MemberMapData"/> for the member being written.</param>
-        /// <returns>The string representation of the object.</returns>
-        public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
-        {
-            if (value is float d && memberMapData.TypeConverterOptions.Formats?.FirstOrDefault() == null)
-            {
-                return d.ToString(defaultFormat, memberMapData.TypeConverterOptions.CultureInfo);
-            }
-
-            return base.ConvertToString(value, row, memberMapData);
-        }
-
-        /// <summary>
-        /// Converts the string to an object.
-        /// </summary>
-        /// <param name="text">The string to convert to an object.</param>
-        /// <param name="row">The <see cref="IReaderRow"/> for the current record.</param>
-        /// <param name="memberMapData">The <see cref="MemberMapData"/> for the member being created.</param>
-        /// <returns>The object created from the string.</returns>
-        public override object ConvertFromString( string text, IReaderRow row, MemberMapData memberMapData )
+		/// <summary>
+		/// Converts the string to an object.
+		/// </summary>
+		/// <param name="text">The string to convert to an object.</param>
+		/// <param name="row">The <see cref="IReaderRow"/> for the current record.</param>
+		/// <param name="memberMapData">The <see cref="MemberMapData"/> for the member being created.</param>
+		/// <returns>The object created from the string.</returns>
+		public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
 		{
 			var numberStyle = memberMapData.TypeConverterOptions.NumberStyle ?? NumberStyles.Float;
 
-			if( float.TryParse( text, numberStyle, memberMapData.TypeConverterOptions.CultureInfo, out var f ) )
+			if (float.TryParse(text, numberStyle, memberMapData.TypeConverterOptions.CultureInfo, out var f))
 			{
 				return f;
 			}
 
-			return base.ConvertFromString( text, row, memberMapData );
+			return base.ConvertFromString(text, row, memberMapData);
 		}
 	}
 }
