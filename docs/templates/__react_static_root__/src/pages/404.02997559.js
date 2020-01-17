fdff2b2d44c64b5328df5f5bb02a8c3f05a0100e
Josh Close <narshe@gmail.com>
2020-01-16 23:30:49 -0600

    Docs build.

---
+++ docs/templates/__react_static_root__/src/pages/404.02997559.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{69:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a);e.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"404 - Oh no's! We couldn\"t find that page :("))}}}]);
---
+++ docs/templates/styles.4b790422.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{137:function(n,o,w){},139:function(n,o,w){}}]);
