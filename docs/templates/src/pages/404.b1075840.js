4fcccda19518388d7bf4aa6819d9ea8416f0c0b9
Josh Close <narshe@gmail.com>
2020-01-16 23:20:49 -0600

    First docs build from react-static upgrade.

--- docs/templates/src/pages/404.b1075840.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{55:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(0),l=function(e){return e&&e.__esModule?e:{default:e}}(u);t.default=function(){return l.default.createElement("div",null,l.default.createElement("h1",null,"404 - Oh no's! We couldn't find that page :("))}}});
-//# sourceMappingURL=404.b1075840.js.map
