1dfce296819cccc966dc5c988e5b2a3578b89b25
Davide <davide.giacometti@outlook.it>
2020-11-03 20:57:00 +0100

    empty CsvDataReader and test

--- tests/CsvHelper.Tests/DataTableTests/CsvDataReaderTests.cs
+++ tests/CsvHelper.Tests/DataTableTests/CsvDataReaderTests.cs
@@ -2,15 +2,14 @@
 // This file is a part of CsvHelper and is dual licensed under MS-PL and Apache 2.0.
 // See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html for MS-PL and http://opensource.org/licenses/Apache-2.0 for Apache 2.0.
 // https://github.com/JoshClose/CsvHelper
-using CsvHelper.Tests.Mocks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
 using System.IO;
 using System.Text;
+using CsvHelper.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CsvHelper.Tests.DataTableTests
 {
@@ -271,6 +270,20 @@ namespace CsvHelper.Tests.DataTableTests
 						dr.GetOrdinal("Foo");
 					});
 				}
+			}
+		}
+
+		[TestMethod]
+		public void DataTableLoadEmptyTest()
+		{
+			var s = "";
+			using (var reader = new StringReader(s.ToString()))
+			using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+			{
+				csv.Configuration.HasHeaderRecord = false;
+
+				var dataReader = new CsvDataReader(csv);
+				Assert.AreEqual(0, dataReader.FieldCount);
 			}
 		}
 	}
