2f806ede42703397610673ccc0b20c2c431c0c3a
Josh Close <narshe@gmail.com>
2021-01-23 09:51:21 -0600

    wip

---
+++ src/CsvHelper.Website/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace CsvHelper.Docs
+{
+	class Program
+	{
+		static async Task<int> Main(string[] args) => await Bootstrapper
+			.Factory
+			.CreateWeb(args)
+			.RunAsync();
+	}
+}
