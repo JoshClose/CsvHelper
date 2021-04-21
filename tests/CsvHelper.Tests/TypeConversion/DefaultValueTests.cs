7787e42463327b8feabcc501beee42eecaeddecf
Josh Close <narshe@gmail.com>
2021-04-21 14:14:02 -0500

    Added default value test.

--- tests/CsvHelper.Tests/TypeConversion/DefaultValueTests.cs
+++ tests/CsvHelper.Tests/TypeConversion/DefaultValueTests.cs
@@ -5,18 +5,19 @@ using CsvHelper.TypeConversion;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace CsvHelper.Tests.TypeConversion.DefaultValueTests
+namespace CsvHelper.Tests.TypeConversion
 {
-	public class BitIntegerConverter_ConvertFromString
+	public class DefaultValueTests
 	{
 		[Fact]
-		public void InvalidValue_UsesDefault()
+		public void BigIntegerConverter_ConvertFromString_InvalidValue_UsesDefault()
 		{
 			var converter = new BigIntegerConverter();
 
@@ -30,9 +31,33 @@ namespace CsvHelper.Tests.TypeConversion.DefaultValueTests
 			Assert.Equal(data.Default, result);
 		}
 
+		[Fact]
+		public void GetRecords_EmptyValue_DefaultSet_UsesDefault()
+		{
+			var s = new StringBuilder();
+			s.Append("Property\r\n");
+			s.Append("foo\r\n");
+			using (var reader = new StringReader(s.ToString()))
+			using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+			{
+				csv.Context.RegisterClassMap<FooMap>();
+				var records = csv.GetRecords<Foo>().ToList();
+
+				Assert.Equal(1, records[0].Property);
+			}
+		}
+
 		private class Foo
 		{
 			public BigInteger Property { get; set; }
+		}
+
+		private class FooMap : ClassMap<Foo>
+		{
+			public FooMap()
+			{
+				Map(m => m.Property).Default(1, useOnConversionFailure: true);
+			}
 		}
 	}
 }
