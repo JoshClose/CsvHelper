ceb48f3959a65b3f84f0c1360f9499e31998e750
Josh Close <narshe@gmail.com>
2022-04-27 11:58:06 -0500

    Updated license headers.

--- tests/CsvHelper.Tests/Issues/Issue1954.cs
+++ tests/CsvHelper.Tests/Issues/Issue1954.cs
@@ -1,4 +1,8 @@
-﻿using CsvHelper.Configuration;
+﻿// Copyright 2009-2022 Josh Close
+// This file is a part of CsvHelper and is dual licensed under MS-PL and Apache 2.0.
+// See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html for MS-PL and http://opensource.org/licenses/Apache-2.0 for Apache 2.0.
+// https://github.com/JoshClose/CsvHelper
+using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
 using System.Globalization;
 using System.IO;
