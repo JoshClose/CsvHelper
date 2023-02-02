e23787cfa7819839800a6326ece42ded63af7d75
Robert Hague <rh@johnstreetcapital.com>
2023-02-02 14:18:27 +0000

    Ensure the ConvertToStringArgs are constructed with the correct generic type
    When the parameterless Map() (returning MemberMap<object, object>) is used in
    combination with Convert(ConvertToString<TClass>) then TClass is object but when
    writing we invoke the delegate with a ConvertToStringArgs<T> where T is the type
    of the record and not object. This fails because TClass is invariant on the
    ConvertToString delegate (we cannot express it as contravariant).
    So we need to construct the ConvertToStringArgs with the same generic type as
    that of the ConvertToString delegate stored in WritingConvertExpression.
    
    fixes #2118

---
+++ tests/CsvHelper.Tests/Issues/Issue2118.cs
@@ -0,0 +1,46 @@
+﻿using CsvHelper.Configuration;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Xunit;
+
+namespace CsvHelper.Tests.Issues
+{
+	public class Issue2118
+	{
+		[Fact]
+		public void Issue2118Test()
+		{
+			var records = new List<Foo>
+			{
+				new() { Bar = new HashSet<string> { "foo" } },
+				new() { Bar = new HashSet<string> { "bar" } },
+			};
+
+			string csvString;
+			using (var writer = new StringWriter())
+			using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+			{
+				csv.Context.RegisterClassMap<FooMap>();
+				csv.WriteRecords(records);
+				csvString = writer.ToString();
+			}
+
+			Assert.Equal("Foo,Bar\r\nTrue,False\r\nFalse,True\r\n", csvString);
+		}
+
+		private class Foo
+		{
+			public HashSet<string> Bar { get; set; }
+		}
+
+		private sealed class FooMap : ClassMap<Foo>
+		{
+			public FooMap()
+			{
+				Map().Index(0).Name("Foo").Convert(x => (x.Value as Foo).Bar.Contains("foo") ? "True" : "False");
+				Map().Index(1).Name("Bar").Convert(x => (x.Value as Foo).Bar.Contains("bar") ? "True" : "False");
+			}
+		}
+	}
+}
