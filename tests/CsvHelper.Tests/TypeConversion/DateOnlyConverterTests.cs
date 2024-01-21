b2878855374e2221e07df03285b12cafdcf38f65
Austin Wise <AustinWise@gmail.com>
2024-01-21 13:08:22 -0800

    Use NET6_0_OR_GREATER to detect support for DateOnly and TimeOnly
    If support is ever added for .NET 7 or later, this will ensure support for these classes is still compiled in.

--- tests/CsvHelper.Tests/TypeConversion/DateOnlyConverterTests.cs
+++ tests/CsvHelper.Tests/TypeConversion/DateOnlyConverterTests.cs
@@ -2,7 +2,7 @@
 // This file is a part of CsvHelper and is dual licensed under MS-PL and Apache 2.0.
 // See LICENSE.txt for details or visit http://www.opensource.org/licenses/ms-pl.html for MS-PL and http://opensource.org/licenses/Apache-2.0 for Apache 2.0.
 // https://github.com/JoshClose/CsvHelper
-#if NET6_0
+#if NET6_0_OR_GREATER
 using CsvHelper.Configuration;
 using CsvHelper.Tests.Mocks;
 using CsvHelper.TypeConversion;
