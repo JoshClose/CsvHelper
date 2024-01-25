16bf2b064ff912f22b88b36734bf9b2ab146d0c8
Josh Close <narshe@gmail.com>
2024-01-25 17:48:40 -0600

    Cleanup.

--- src/CsvHelper/TypeConversion/CollectionConverterFactory.cs
+++ src/CsvHelper/TypeConversion/CollectionConverterFactory.cs
@@ -6,10 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CsvHelper.TypeConversion
 {
